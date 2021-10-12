9a7ef8c7727126c535bf2d2d38cdb3deabe98ff2
Hardy Rafael jimenez Matos <hardy.rafael17@gmail.com>
2021-10-12 08:40:09 +0200

    update

--- ft_split.c
+++ ft_split.c
@@ -6,32 +6,17 @@
 /*   By: hjimenez <hjimenez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 08:44:46 by hjimenez          #+#    #+#             */
-/*   Updated: 2021/10/10 10:03:38 by hjimenez         ###   ########.fr       */
+/*   Updated: 2021/10/11 07:39:29 by hjimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+//get the amount of substrings to be created
+//allocate memory for them (Plus the null one)
+//iterate over given array
 #include "libft.h"
 
 char	**ft_split(char const *s, char c)
 {
-	char	**strarr;
-	size_t	i;
-	char	*spointer;
 
-	strarr = ft_calloc(i, sizeof(char *));
-	size_t	compare;
-	spointer = (char *)s;
-	compare = 0;
-
-	while (*(spointer + compare) == c || *(spointer + compare))
-	{
-		while (*(spointer + compare + 1) != c && *(spointer + compare + 1))
-		{
-			spointer = ft_strchr(spointer + 1, c);
-		}
-		compare++;
-	}
-	i = 1;
-	strarr = ft_calloc(i, sizeof(char *));
-	return (strarr);
 }
