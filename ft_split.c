aa7b6fb59d070be75e120e670d89383e77b12c0c
Hardy Rafael jimenez Matos <hardy.rafael17@gmail.com>
2021-10-13 09:56:01 +0200

    added slplit.c

--- ft_split.c
+++ ft_split.c
@@ -6,17 +6,66 @@
 /*   By: hjimenez <hjimenez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 08:44:46 by hjimenez          #+#    #+#             */
-/*   Updated: 2021/10/11 07:39:29 by hjimenez         ###   ########.fr       */
+/*   Updated: 2021/10/13 01:41:14 by hjimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
-//get the amount of substrings to be created
-//allocate memory for them (Plus the null one)
-//iterate over given array
 #include "libft.h"
 
 char	**ft_split(char const *s, char c)
 {
 
+	size_t	words;
+	size_t	i;
+	char	**arr;
+	size_t	wordLength;
+	
+	if (!s)
+		return (NULL);
+	words = 0;
+	i = 0;
+	wordLength = 0;
+	while (*(s + i))
+	{
+		if (*(s + i) != c)
+		{
+			wordLength++;
+			if (wordLength == 1)
+			{
+				words++;
+			}
+		}
+		if (wordLength > 0 && (*(s + i + 1)== 0 || *(s + i + 1) == c))
+		{
+			wordLength = 0;
+		}
+		i++;
+	}
+	arr = ft_calloc(words + 1, sizeof(char *));
+	if (!arr)
+		return (NULL);
+	words = 0;
+	i = 0;
+	wordLength = 0;
+	while (*(s + i))
+	{
+		if (*(s + i) != c)
+		{
+			wordLength++;
+			if (wordLength == 1)
+			{
+				words++;
+			}
+		}
+		if (wordLength > 0 && (*(s + i + 1)== 0 || *(s + i + 1) == c))
+		{
+
+			arr[words - 1]= ft_substr(s, i - wordLength +1, wordLength);
+			wordLength = 0;
+		}
+		i++;
+	}
+
+	return (arr);
 }
