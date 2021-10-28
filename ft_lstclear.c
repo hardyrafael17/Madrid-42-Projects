c564827bc6534d7b76aee87e45c318f250b457b2
Hardy Jimenez <hjimenez@hjimenez.local>
2021-10-28 12:32:10 +0200

    fixed ft_sltclear.c

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -15,13 +15,14 @@
 void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
 	t_list	*tmp;
-
 	if (!del || !lst || !*lst)
 		return ;
 	while (lst && *lst)
 	{
 		tmp = (*lst)->next;
-		ft_lstdelone(*lst, del);
+		del((*lst)->content);
+		free(*lst);
 		*lst = tmp;
 	}
 }
+
