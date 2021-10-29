b189bce520dc807f7b9a115ed6c4eb7a7e53a5c6
Hardy Rafael Jimenez Matos <hardy.rafael17@gmail.com>
2021-10-29 10:01:17 +0200

    added final steps

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: hjimenez <hjimenez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/21 15:03:37 by hjimenez          #+#    #+#             */
-/*   Updated: 2021/10/28 23:05:56 by hjimenez         ###   ########.fr       */
+/*   Updated: 2021/10/29 08:46:10 by hjimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,9 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 		{
 			*lst = new;
 		}	
+	}
+	else if (lst == NULL)
+	{
+		lst = &new;
 	}
 }
--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,34 +6,37 @@
 /*   By: hjimenez <hjimenez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 23:06:06 by hjimenez          #+#    #+#             */
-/*   Updated: 2021/10/28 23:06:07 by hjimenez         ###   ########.fr       */
+/*   Updated: 2021/10/29 09:40:22 by hjimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "libft.h"
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+	t_list	*new_list;
 	t_list	*new_node;
-	t_list	**first_node;
 
 	if (!lst || !f)
 		return (NULL);
-	first_node = NULL;
+	if (!(new_node = ft_lstnew(f(lst->content))))
+	{
+		ft_lstclear(&lst, del);
+		return (NULL);
+	}
+	new_list = new_node;
+	lst = lst->next;
 	while (lst)
 	{
-		new_node = ft_lstnew(f(lst->content));
-		if (!new_node)
+		if (!(new_node = ft_lstnew(f(lst->content))))
 		{
-			ft_lstclear(first_node, del);
-			return (NULL);
+			ft_lstclear(&lst, del);
+			ft_lstclear(&new_list, del);
+			break ;
 		}
-		if (!first_node)
-		{
-			first_node = &new_node;
-		}
-		ft_lstadd_back(first_node, new_node);
 		lst = lst->next;
+		ft_lstadd_back(&new_list, new_node);
 	}
-	return (*first_node);
+	return (new_list);
 }
